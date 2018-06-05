0ddd7807b91d23c44ebb46fa2118dbef6efc7c22
Axel DHONDT <adhondt@e1r11p19.42.fr>
2018-06-05 12:30:54 +0200

    auto commit

--- main.c
+++ main.c
@@ -6,13 +6,11 @@ int i;
 int j;
 char *str;
 
-/*	str = "TEST :42%+02.1zd42!\n";
-	i = ft_printf(str, 0);
-	j = printf(str, 0);
+	str = "!%%-i 42 == %-i!\n";
+	i = ft_printf(str, 42);
+	j = printf(str, 42);
 	printf("i:%d, j:%d\n", i, j);
 	//i = ft_printf(str, 4);
 	//j = printf(str, 4);
-*/
-	ft_printf("%%d Lydie == |%d|", 503);
-	ft_printf("\n");
+
 }
