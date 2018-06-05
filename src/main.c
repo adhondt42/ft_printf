bf06921f1db5ab0c1d0e5d42b12fe6341877d786
Axel DHONDT <adhondt@e1r11p19.42.fr>
2018-06-05 14:56:04 +0200

    auto commit

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: adhondt <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/26 12:15:41 by adhondt           #+#    #+#             */
-/*   Updated: 2018/06/05 14:54:34 by adhondt          ###   ########.fr       */
+/*   Updated: 2018/06/05 14:55:57 by adhondt          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,9 +66,9 @@ int			ft_printf(const char *str, ...)
 	ret = 0;
 	va_start(va, str);
 	i = -1;
-	printf("Here:%s|\n", str);
+/*	printf("Here:%s|\n", str);
 	printf("Here:%i|\n", va_arg(va, int));
-	/*while (str[++i])
+*/	while (str[++i])
 	{
 		if (str[i] == '%')
 		{
@@ -85,4 +85,4 @@ int			ft_printf(const char *str, ...)
 			(ret++) ? ft_putchar(str[i]) : ft_putchar(str[i]);
 	}
 	return (ret);
-*/}
+}
